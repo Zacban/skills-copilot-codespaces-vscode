fdf9f4661c67ec97adcb42e71ae1a7dc92171267
Jesper Raundahl <zacban@gmail.com>
2024-06-10 20:38:47 +0000

    Copilot third message

---
+++ comments.js
@@ -0,0 +1,46 @@
+// create web server using express
+var express = require('express');
+var app = express();
+var fs = require('fs');
+
+// create a server
+var server = app.listen(3000, function() {
+	console.log('Server is running on port 3000');
+});
+
+// use the public folder to serve static files
+app.use(express.static('public'));
+
+// use the body-parser module
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+// read the comments from the file
+var comments;
+fs.readFile('comments.json', function(err, data) {
+	if (err) {
+		console.log(err);
+	} else {
+		comments = JSON.parse(data);
+	}
+});
+
+// get the comments
+app.get('/getComments', function(req, res) {
+	res.send(comments);
+});
+
+// post a comment
+app.post('/postComment', function(req, res) {
+	var newComment = req.body;
+	comments.push(newComment);
+	fs.writeFile('comments.json', JSON.stringify(comments), function(err) {
+		if (err) {
+			console.log(err);
+		} else {
+			res.send('success');
+		}
+	});
+});
+
